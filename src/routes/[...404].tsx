2bb289e71b660fc81f57bd2204fd2f1a21a3c67f
dejwf <david@zitko.org>
2023-01-20 17:40:12 +0100

    Feat(:tada:) udate static content
    initial changes to have the site in CZE

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,13 +6,13 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Tady nic neni</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        Jdi zpatky na{" "}
+        <a href="/">
+          domovskou stranku
         </a>{" "}
-        to learn how to build SolidStart apps.
+        aby jsi se mohl vratit zpatky do hry.
       </p>
     </main>
   );
